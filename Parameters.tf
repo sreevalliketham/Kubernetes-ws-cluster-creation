229109b0d10580a8c30806e2cb269f205fec546f
sreevalliketham <sreevalliketham@gmail.com>
2025-07-24 17:25:09 +0530

    Update Parameters.tf

--- Parameters.tf
+++ Parameters.tf
@@ -1,13 +1,1 @@
-resource "aws_ssm_parameter" "access_key" {
-  name        = "my-access-key"
-  type        = "SecureString"
-  value       = var.access_key
-  overwrite   = true
-}
 
-resource "aws_ssm_parameter" "secret_key" {
-  name        = "my-secret-key"
-  type        = "SecureString"
-  value       = var.secret_key
-  overwrite   = true
-}
