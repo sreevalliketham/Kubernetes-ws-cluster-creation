d78b8a6deb0765e97a51d8e6a73affc6df2546ef
Sreevalli Ketham <youremail@example.com>
2025-07-24 17:52:00 +0530

    k8rcluster

--- variables.tf
+++ variables.tf
@@ -5,22 +5,3 @@ variable "project" {
 variable "environment" {
     default = "dev"
 }
-
-<<<<<<< HEAD
-variable "aws_region" {
-  type        = string
-  description = "AWS region"
-  default     = "us-east-1"
-=======
-variable "access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
->>>>>>> 229109b0d10580a8c30806e2cb269f205fec546f
-}
